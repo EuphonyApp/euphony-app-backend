235060a6da27a0a6c392c618b1cc217f7f21deac
Pro gupta <pchemistry.35@gmail.com>
2016-07-19 13:52:19 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -121,7 +121,7 @@ module.exports = function(app) {
 					res.end(err);
 				else if(docs.length != 0) {
 					console.log(req.query.f_id + " " + docs[0].__t);
-					res.json(docs[0]._id + "," + docs[0].__t + "," + docs[0].location + "," + docs[0].name); 
+					res.json(docs[0]._id + "-" + docs[0].__t + "-" + docs[0].location + "-" + docs[0].name); 
 				} else if(docs.length == 0) {
 					console.log(req.query.f_id + " not found");
 					res.json("null");
@@ -133,7 +133,7 @@ module.exports = function(app) {
 					res.end(err);
 				else if(docs.length != 0) {
 					console.log(req.query.g_id + " " + docs[0]._id);
-					res.json(docs[0]._id + "," + docs[0].__t + "," + docs[0].location + "," + docs[0].name);
+					res.json(docs[0]._id + "-" + docs[0].__t + "-" + docs[0].location + "-" + docs[0].name);
 				} else if(docs.length == 0) {
 					console.log(req.query.g_id + " not fond");
 					res.json("null");
